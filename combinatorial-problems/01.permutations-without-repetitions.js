189e468f9a3f36de3790b477fc748ed3091fe868
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-11-30 14:47:47 +0200

    Permutations without Repetitions

---
+++ combinatorial-problems/01.permutations-without-repetitions.js
@@ -0,0 +1,23 @@
+function permutationWithoutRepetitions(arr) {
+
+    const result = [];
+    permutation(0);
+
+    function permutation(index) {
+        if (index >= arr.length) {
+            console.log(result);
+            return;
+        }
+        for (let i = 0; i < arr.length; i++) {
+            if (!result.includes(arr[i])) {
+                result.push(arr[i]);
+                permutation(index + 1);
+                result.pop();
+            }
+        }
+    }
+}
+
+permutationWithoutRepetitions(['A', 'B']);
+permutationWithoutRepetitions(['A', 'B', 'C']);
+permutationWithoutRepetitions(['A', 'B', 'C', 'D']);
