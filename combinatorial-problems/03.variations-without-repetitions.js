23c315ddf3ad4e7a206dee49d92fc5710aea22c2
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-12-01 12:05:56 +0200

    Variations Without Repetitions

---
+++ combinatorial-problems/03.variations-without-repetitions.js
@@ -0,0 +1,23 @@
+function variationsWithoutRepetitions(arr, k){
+
+    const result = Array(k);
+    variations(0);
+
+    function variations(index){
+        if(index >= k){
+            console.log(result.join(' '));
+            return;
+        }
+
+        for(let i = 0; i <= arr.length; i++) {
+            if(!result.includes(arr[i])){
+                result.push(arr[i]);
+                variations(index + 1);
+                result.pop();
+            }
+        }
+    }
+}
+
+variationsWithoutRepetitions(['A', 'B', 'C'], 2);
+variationsWithoutRepetitions(['A', 'B', 'C', 'D'], 2);
