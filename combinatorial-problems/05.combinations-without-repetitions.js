c522bedd5a304136c31200fd7d584e3437e1e5be
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-12-01 12:39:26 +0200

    Combinations

--- combinatorial-problems/05.combinations-without-repetitions.js
+++ combinatorial-problems/05.combinations-without-repetitions.js
@@ -1,15 +1,15 @@
-function combinationsWithoutRepetitions(arr, k){
+function combinationsWithoutRepetitions(arr, k) {
     const result = Array(k);
     combinations(0, 0);
 
-    function combinations(index, start){
-        if(index >= k){
+    function combinations(index, start) {
+        if (index >= k) {
             console.log(result.join(' '));
             return;
         }
-        for(let i = start; i < arr.length; i++) {
-                result[index] = arr[i];
-                combinations(index + 1, i + 1);
+        for (let i = start; i < arr.length; i++) {
+            result[index] = arr[i];
+            combinations(index + 1, i + 1);
         }
     }
 }
---
+++ combinatorial-problems/06.combinations-with-repetitions.js
@@ -0,0 +1,19 @@
+function combinationsWithoutRepetitions(arr, k) {
+    const result = Array(k);
+    combinations(0, 0);
+
+    function combinations(index, start) {
+        if (index >= k) {
+            console.log(result.join(' '));
+            return;
+        }
+        for (let i = start; i < arr.length; i++) {
+            result[index] = arr[i];
+            combinations(index + 1, i);
+        }
+    }
+}
+
+combinationsWithoutRepetitions(['A', 'B', 'C'], 2);
+// combinationsWithoutRepetitions(['A', 'B', 'C', 'D'], 2);
+// combinationsWithoutRepetitions(['A', 'B', 'C', 'D', 'E'], 3);
