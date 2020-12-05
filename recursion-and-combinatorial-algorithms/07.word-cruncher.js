e051349d253e60d5e4ce4f8219d427976e04439a
BoykoPetevBoev <boykopetevboev@gmail.com>
2020-12-05 15:22:21 +0200

    Word cruncher

--- recursion-and-combinatorial-algorithms/07.word-cruncher.js
+++ recursion-and-combinatorial-algorithms/07.word-cruncher.js
@@ -1,31 +1,26 @@
 function wordCruncher(arr, word) {
-    console.log('- start');
     const result = [];
     const words = [...new Set(arr)];
+    const printResult = () => console.log(result.join(' '));
 
     recursion(0)
-    console.log('- end');
     function recursion(index) {
-        if (word === result.join('')) {
-            console.log(result.join(' '));
+        const end = word === result.join('');
+        const match = word.startsWith(result.join(''));
+
+        if (end) {
+            printResult();
             return;
         }
-        for (let i = 0; i < words.length; i++) {
-            if (result.includes(words[i])) continue;
-
-            result.push(words[i]);
-
-            if (word.startsWith(result.join(''))) {
-                recursion(index + 1);
-            }
+        for (let string of words) {
+            if (result.includes(string)) continue;
+            result.push(string);
+            if (match) recursion(index + 1);
             result.pop();
-
         }
     }
 }
 
-wordCruncher(['text', 'me', 'so', 'do', 'm', 'ran'], 'somerandomtext')
-
-wordCruncher(['Word', 'cruncher', 'cr', 'h', 'unch', 'c', 'r', 'un', 'ch', 'er'], 'Wordcruncher')
-
-wordCruncher(['tu', 'stu', 'p', 'i', 'd', 'pi', 'pid', 's', 'pi'], 'stupid')
+wordCruncher(['text', 'me', 'so', 'do', 'm', 'ran'], 'somerandomtext');
+wordCruncher(['Word', 'cruncher', 'cr', 'h', 'unch', 'c', 'r', 'un', 'ch', 'er'], 'Wordcruncher');
+wordCruncher(['tu', 'stu', 'p', 'i', 'd', 'pi', 'pid', 's', 'pi'], 'stupid');
